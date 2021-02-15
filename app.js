77e78ed1725cf246e907a5d40bc6376296e004f9
Allysson Vieira <allysson.v1@gmail.com>
2021-02-15 20:27:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
+const fs = require('fs')
 const app = express()
+
 
 const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 const IP = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0';
 
-
 app.get('/', (req, res) => {
-    res.send(cool())
+
+    fs.readFile('/tmp/file.txt', (err, data) => {
+        if (err) throw err
+        res.send(data.toString())
+    })
+
+
+    //res.send(cool())
 })
 
 app.listen(PORT, () => {
