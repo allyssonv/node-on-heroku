f908964300812cbe49e7488117d4a5bf8614bada
Allysson Vieira <allysson.v1@gmail.com>
2021-02-15 13:30:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app = express()
 
 const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
+const IP = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0';
+
+
 app.get('/', (req, res) => {
     res.send(cool())
 })
 
 app.listen(PORT, () => {
-    console.log(`Server listen on ${PORT}`)
+    console.log(`Server listen on ${IP}:${PORT}`)
 })
